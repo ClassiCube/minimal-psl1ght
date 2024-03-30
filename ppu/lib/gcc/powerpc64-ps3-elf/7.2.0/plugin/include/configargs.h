b96441812b8cf15a29c786dd19166ba41e12aa2c
UnknownShadow200 <unknownshadow200@gmail.com>
2024-03-30 22:14:28 +1100

    Add everything

---
+++ ppu/lib/gcc/powerpc64-ps3-elf/7.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/home/minty/ConsoleSDK/ps3dev/ppu --target=powerpc64-ps3-elf --disable-dependency-tracking --disable-libcc1 --disable-libstdcxx-pch --disable-multilib --disable-nls --disable-shared --disable-win32-registry --enable-languages=c,c++ --enable-long-double-128 --enable-lto --enable-threads --with-cpu=cell --with-newlib --enable-newlib-multithread --enable-newlib-hw-fp --with-system-zlib : (reconfigured) ../configure --prefix=/home/minty/ConsoleSDK/ps3dev/ppu --target=powerpc64-ps3-elf --disable-dependency-tracking --disable-libcc1 --disable-libstdcxx-pch --disable-multilib --disable-nls --disable-shared --disable-win32-registry --enable-languages=c,c++ --enable-long-double-128 --enable-lto --enable-threads --with-cpu=cell --with-newlib --enable-newlib-multithread --enable-newlib-hw-fp --with-system-zlib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cell" } };
